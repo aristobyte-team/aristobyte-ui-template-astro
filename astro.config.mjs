e41a926db1fe7975ff664c0dbc91f9540eee6ec6
aristobyte <info@aristobyte.com>
2025-09-08 02:25:40 +0200

    feat: add pnpm config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,2 +1,7 @@
 import { defineConfig } from "astro/config";
-export default defineConfig({});
+import tailwind from "@tailwindcss/vite";
+import react from "@astrojs/react";
+
+export default defineConfig({
+  integrations: [react(), tailwind({ config: "./tailwind.config.ts" })],
+});
